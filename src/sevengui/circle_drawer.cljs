b362563cc18655dffa00f6a0c21b7069aaa80b45
Shan Rauf <s.rauf124@gmail.com>
2021-07-10 01:39:48 -0700

    Pass around state instead of using global state on task 6

--- src/sevengui/circle_drawer.cljs
+++ src/sevengui/circle_drawer.cljs
@@ -14,16 +14,16 @@
 
 ;; -------------------------
 (defonce canvas-state
-  (r/atom {:circles []
-           :selected-circle nil
-           :undo-stack []
-           :redo-stack []
-           :popup-active false
-           :slider-active false}))
+  {:circles []
+   :selected-circle nil
+   :undo-stack []
+   :redo-stack []
+   :popup-active false
+   :slider-active false})
 
 ;; Computed properties
-(defn- selected? [c]
-  (= (:id c) (:id (:selected-circle @canvas-state))))
+(defn- selected? [state c]
+  (= (:id c) (:id (:selected-circle @state))))
 
 (defn- same-circles? [c1 c2]
   (= (:id c1) (:id c2)))
@@ -31,54 +31,50 @@
 (defn- equivalent-circles? [c1 c2]
   (= (:r c1) (:r c2)))
 
-(defn- some-circle-selected? []
-  (boolean (:selected-circle @canvas-state)))
+(defn- some-circle-selected? [state]
+  (boolean (:selected-circle @state)))
 
-(defn- popup-active? []
-  (:popup-active @canvas-state))
+(defn- popup-active? [state]
+  (:popup-active @state))
 
-(defn- slider-active? []
-  (:slider-active @canvas-state))
+(defn- slider-active? [state]
+  (:slider-active @state))
 
-(defn- can-undo?
-  []
-  (seq (:undo-stack @canvas-state)))
+(defn- can-undo? [state]
+  (seq (:undo-stack @state)))
 
-(defn- can-redo?
-  []
-  (seq (:redo-stack @canvas-state)))
+(defn- can-redo? [state]
+  (seq (:redo-stack @state)))
 
 ;; -------------------------
 ;; Canvas History
 
 ;; -------------------------
-(defn- undo!
-  []
-  (let [{:keys [undo-stack redo-stack circles]} @canvas-state]
-    (when (can-undo?)
-      (swap! canvas-state assoc
+(defn- undo! [state]
+  (let [{:keys [undo-stack redo-stack circles]} @state]
+    (when (can-undo? state)
+      (swap! state assoc
              :popup-active false
              :selected-circle nil
              :circles (last undo-stack)
              :undo-stack (pop undo-stack)
              :redo-stack (conj redo-stack circles)))))
 
-(defn- redo!
-  []
-  (let [{:keys [undo-stack redo-stack circles]} @canvas-state]
-    (when (can-redo?)
-      (swap! canvas-state assoc
+(defn- redo! [state]
+  (let [{:keys [undo-stack redo-stack circles]} @state]
+    (when (can-redo? state)
+      (swap! state assoc
              :popup-active false
              :selected-circle nil
              :circles (last redo-stack)
              :redo-stack (pop redo-stack)
              :undo-stack (conj undo-stack circles)))))
 
-(defn- set-undo-stack! [s]
-  (swap! canvas-state assoc :undo-stack s))
+(defn- set-undo-stack! [state stack]
+  (swap! state assoc :undo-stack stack))
 
-(defn- set-redo-stack! [s]
-  (swap! canvas-state assoc :redo-stack s))
+(defn- set-redo-stack! [state stack]
+  (swap! state assoc :redo-stack stack))
 
 ;; -------------------------
 ;; Circle
@@ -95,7 +91,7 @@
                 (Math/pow (- x2 x1) 2))))
 
 (defn- get-closest-circle
-  [[x0 y0]]
+  [state [x0 y0]]
   (->
    (reduce
     (fn [prev c]
@@ -108,7 +104,7 @@
           {:d distance-from-circle :c c}
           prev)))
     {:d ##Inf :c nil}
-    (:circles @canvas-state))
+    (:circles @state))
    :c))
 
 (defn- generate-circle [id x y r]
@@ -133,16 +129,16 @@
         y (- (.-clientY e) (-> rect .-top int))]
     (list x y)))
 
-(defn- set-circles! [circles]
-  (swap! canvas-state assoc :circles circles))
+(defn- set-circles! [state circles]
+  (swap! state assoc :circles circles))
 
 (defn- create-circle!
-  [[x y]]
-  (let [{:keys [circles undo-stack]} @canvas-state
+  [state [x y]]
+  (let [{:keys [circles undo-stack]} @state
         new-circle (generate-circle nil x y default-radius)]
-    (set-circles! (conj circles new-circle))
-    (set-undo-stack! (conj undo-stack circles))
-    (set-redo-stack! [])
+    (set-circles! state (conj circles new-circle))
+    (set-undo-stack! state (conj undo-stack circles))
+    (set-redo-stack! state [])
     new-circle))
 
 
@@ -150,113 +146,115 @@
   (and (same-circles? c1 c2)
        (not (equivalent-circles? c1 c2))))
 
-(defn- commit-circle-diameter! []
-  (let [{:keys [selected-circle circles undo-stack]} @canvas-state]
-    (set-circles! (map #(if (adjust-diameter? % selected-circle)
-                          selected-circle %) circles))
-    (set-undo-stack! (conj undo-stack circles))
-    (set-redo-stack! [])))
+(defn- commit-circle-diameter! [state]
+  (let [{:keys [selected-circle circles undo-stack]} @state]
+    (set-circles! state (map #(if (adjust-diameter? % selected-circle)
+                                selected-circle %) circles))
+    (set-undo-stack! state (conj undo-stack circles))
+    (set-redo-stack! state [])))
 
-(defn- set-selected-circle! [c]
-  (swap! canvas-state assoc :selected-circle c))
+(defn- set-selected-circle! [state c]
+  (swap! state assoc :selected-circle c))
 
-(defn- select-closest-circle! [coords]
-  (-> coords
-      (get-closest-circle)
-      ((fn [c] (when (not (selected? c))
-                 (set-selected-circle! c))))))
+(defn- select-closest-circle! [state coords] ; TODO ->> ???
+  (->> coords
+       (get-closest-circle state)
+       ((fn [c] (when (not (selected? state c))
+                  (set-selected-circle! state c))))))
 
 ;; -------------------------
 ;; Popup
 
 ;; -------------------------
-(defn- set-popup! [is-visible]
-  (swap! canvas-state assoc :popup-active is-visible))
+(defn- set-popup! [state is-visible]
+  (swap! state assoc :popup-active is-visible))
 
-(defn- set-slider! [is-visible]
-  (swap! canvas-state assoc :slider-active is-visible))
+(defn- set-slider! [state is-visible]
+  (swap! state assoc :slider-active is-visible))
 
 ;; -------------------------
 ;; Controllers
 
 ;; -------------------------
-(defn- on-canvas-left-click! [e]
-  (-> (get-dom-event-coords e)
-      (create-circle!)
-      (set-selected-circle!)))
+(defn- on-canvas-left-click! [state e]
+  (->> (get-dom-event-coords e)
+       (create-circle! state)
+       (set-selected-circle! state)))
 
-(defn- on-canvas-right-click! [e]
+(defn- on-canvas-right-click! [state e]
   (.preventDefault e)
-  (when (some-circle-selected?) (set-popup! true)))
+  (when (some-circle-selected? state) (set-popup! state true)))
 
-(defn- on-canvas-mouse-move! [e]
-  (when (not (popup-active?))
-    (-> (get-dom-event-coords e)
-        (select-closest-circle!))))
+(defn- on-canvas-mouse-move! [state e]
+  (when (not (popup-active? state))
+    (->> (get-dom-event-coords e)
+         (select-closest-circle! state))))
 
-(defn- on-change-diameter! [d]
-  (let [{:keys [id x y]} (:selected-circle @canvas-state)
+(defn- on-change-diameter! [state d]
+  (let [{:keys [id x y]} (:selected-circle @state)
         r (d->r d)]
-    (set-selected-circle! (generate-circle id x y r))))
+    (set-selected-circle! state (generate-circle id x y r))))
 
-(defn- on-popup-blur! []
-  (commit-circle-diameter!)
-  (set-selected-circle! nil)
-  (set-slider! false)
-  (set-popup! false))
+(defn- on-popup-blur! [state]
+  (commit-circle-diameter! state)
+  (set-selected-circle! state nil)
+  (set-slider! state false)
+  (set-popup! state false))
 
-(defn- on-undo! []
-  (undo!))
+(defn- on-undo! [state]
+  (undo! state))
 
-(defn- on-redo! []
-  (redo!))
+(defn- on-redo! [state]
+  (redo! state))
 
 ;; -------------------------
 ;; View
 
 ;; -------------------------
-(defn- popup-component []
-  (let [{:keys [x y r]} (:selected-circle @canvas-state)]
+(defn- popup-component [state]
+  (let [{:keys [x y r]} (:selected-circle @state)]
     [:form.popup {:style {:left x
                           :top y}
-                  :on-blur #(on-popup-blur!)}
-     (if (slider-active?)
+                  :on-blur #(on-popup-blur! state)}
+     (if (slider-active? state)
        [:div.slider
         [:h3 "Adjust Diameter"]
         [:input {:type "range"
                  :min minimum-radius
                  :auto-focus true ; makes on-blur event fire
                  :value (r->d r)
-                 :on-change #(on-change-diameter! (.. % -target -value))}]]
+                 :on-change #(on-change-diameter! state (.. % -target -value))}]]
        [:button.custom-button {:type "button"
                                :auto-focus true ; makes on-blur event fire
-                               :on-click #(set-slider! true)}
+                               :on-click #(set-slider! state true)}
         "Adjust Diameter..."])]))
 
-(defn- circle-component [c]
+(defn- circle-component [state c]
   (let [{:keys [id x y r]} c
-        is-selected (selected? c)]
+        is-selected (selected? state c)]
     [:circle {:class (when is-selected "selected-circle")
               :key id
               :cx x
               :cy y
-              :r (if is-selected (:r (:selected-circle @canvas-state)) r)}]))
+              :r (if is-selected (:r (:selected-circle @state)) r)}]))
 
-(defn- canvas-component []
+(defn- canvas-component [state]
   [:div.canvas-container
-   [:svg {:on-click #(on-canvas-left-click! %)
-          :on-mouse-move #(on-canvas-mouse-move! %)
-          :on-context-menu #(on-canvas-right-click! %)}
-    (doall (map circle-component (:circles @canvas-state)))]
-   (when (popup-active?) [popup-component])])
+   [:svg {:on-click #(on-canvas-left-click! state %)
+          :on-mouse-move #(on-canvas-mouse-move! state %)
+          :on-context-menu #(on-canvas-right-click! state %)}
+    (doall (map #(circle-component state %) (:circles @state)))]
+   (when (popup-active? state) [popup-component state])])
 
 (defn circle-drawer-component []
-  [:div {:class "task"}
-   [:h2 "Task 6: Circle Drawer"]
-   [:button.custom-button {:type "button"
-                           :disabled (not (can-undo?))
-                           :on-click #(on-undo!)} "Undo"]
-   [:button.custom-button {:type "button"
-                           :disabled (not (can-redo?))
-                           :on-click #(on-redo!)} "Redo"]
-   [canvas-component]])
+  (let [state (r/atom canvas-state)]
+    (fn []
+      [:div {:class "task"}
+       [:h2 "Task 6: Circle Drawer"]
+       [:button.custom-button {:type "button"
+                               :disabled (not (can-undo? state))
+                               :on-click #(on-undo! state)} "Undo"]
+       [:button.custom-button {:type "button"
+                               :disabled (not (can-redo? state))
+                               :on-click #(on-redo! state)} "Redo"]
+       [canvas-component state]])))
