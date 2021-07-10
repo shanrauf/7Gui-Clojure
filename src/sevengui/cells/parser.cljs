6bf2e2d6ad4e803ac2455d665acd8cbb66c2bc5d
Shan Rauf <s.rauf124@gmail.com>
2021-07-10 01:11:25 -0700

    Throw error for unbalanced operations

--- src/sevengui/cells/parser.cljs
+++ src/sevengui/cells/parser.cljs
@@ -2,7 +2,8 @@
   (:require [clojure.string :as str]
             [cljs.reader :as reader]
             [sevengui.views.cells.operator :refer [has-operator?
-                                                   operator-regex]]))
+                                                   operator-regex]]
+            [sevengui.views.cells.error :refer [throw-error value-error]]))
 
 (defn- special-form? [expr]
   (try
@@ -10,7 +11,17 @@
       (not (or (string? result) (number? result))))
     (catch :default _ true)))
 
+(defn balanced?
+  ([expr] (balanced? (str/split expr #"") 0))
+  ([[x & xs] count]
+   (cond (neg? count) false
+         (nil? x) (zero? count)
+         (= x "(") (recur xs (inc count))
+         (= x ")") (recur xs (dec count))
+         :else (recur xs count))))
+
 (defn generate-s-expression [expr]
+  (when (not (balanced? expr)) (throw-error value-error expr))
   (if (has-operator? expr)
     (generate-s-expression (str/replace expr operator-regex "($1, $2)"))
     expr))
