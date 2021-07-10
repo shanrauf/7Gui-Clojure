d4cd221aec430745d34ee5d45e39b976ebe47cc4
Shan Rauf <s.rauf124@gmail.com>
2021-07-10 01:19:36 -0700

    Re-add special form validation to fully match Excel behavior

--- src/sevengui/cells/parser.cljs
+++ src/sevengui/cells/parser.cljs
@@ -1,9 +1,12 @@
 (ns sevengui.views.cells.parser
   (:require [clojure.string :as str]
             [cljs.reader :as reader]
+            [sevengui.views.cells.cell-ref :refer [is-ref-range?]]
             [sevengui.views.cells.operator :refer [has-operator?
                                                    operator-regex]]
             [sevengui.views.cells.error :refer [throw-error value-error]]))
+
+(defonce list-regex #"^(\s*\(.*\)\s*)$")
 
 (defn- special-form? [expr]
   (try
@@ -20,14 +23,24 @@
          (= x ")") (recur xs (dec count))
          :else (recur xs count))))
 
+(defonce special-form-rules
+  {:no-range-outside-operator #(not (is-ref-range? %))
+   :no-list-outside-operator #(not (re-seq list-regex %))
+   :balanced-operations balanced?})
+
+(defn- validate-special-form [expr]
+  (if (every? true? (map #(% expr) (vals special-form-rules)))
+    expr
+    (throw-error value-error expr)))
+
 (defn generate-s-expression [expr]
-  (when (not (balanced? expr)) (throw-error value-error expr))
   (if (has-operator? expr)
     (generate-s-expression (str/replace expr operator-regex "($1, $2)"))
     expr))
 
 (defn- parse-special-form [expr]
   (->> expr
+       (validate-special-form)
        (generate-s-expression)
        (reader/read-string)))
 
